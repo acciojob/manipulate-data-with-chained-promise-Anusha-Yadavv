ee92ca71d26d6cee8192002148a54af16422c24b
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-21 20:00:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+// script.js
+function manipulateArray(arr) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(arr);
+    }, 3000);
+  });
+}
+
+function filterOddNumbers(arr) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const filteredArray = arr.filter((num) => num % 2 === 0);
+      resolve(filteredArray);
+    }, 1000);
+  });
+}
+
+function multiplyEvenNumbers(arr) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const multipliedArray = arr.map((num) => (num % 2 === 0 ? num * 2 : num));
+      resolve(multipliedArray);
+    }, 2000);
+  });
+}
+
+function updateOutputText(text) {
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = text;
+}
+
+const inputArray = [1, 2, 3, 4];
+
+manipulateArray(inputArray)
+  .then(filterOddNumbers)
+  .then(multiplyEvenNumbers)
+  .then((resultArray) => {
+    updateOutputText(resultArray.join(", "));
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
